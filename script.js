50cbaf4fd4073acf121320ea2b690d6da564b772
Angelo-sabatini <lucas.sabatini69@gmail.com>
2026-04-26 20:40:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,19 +5,31 @@ for (let h = 1; h <= 24; h++) {
 
 const tablebody = document.getElementById("table-body");
 
-hours.forEach(hour => {
+const savedData = JSON.parse(localStorage.getItem("tableData")) || {};
+
+hours.forEach((hour, rowIndex) => {
     const row = document.createElement("tr");
 
     const hourCell = document.createElement("td");
     hourCell.innerText = hour;
     row.appendChild(hourCell);
 
-    for (let i = 0; i < 7; i++) {
+    for (let colIndex = 0; colIndex < 7; colIndex++) {
         const cell = document.createElement("td");
         cell.contentEditable = true;
+
+        const key = `${rowIndex}-${colIndex}`;
+        if (savedData[key]) {
+            cell.innerText = savedData[key];
+        }
+
+        cell.addEventListener("input", () => {
+            savedData[key] = cell.innerText;
+            localStorage.setItem("tableData", JSON.stringify(savedData));
+        });
+
         row.appendChild(cell);
     }
 
     tablebody.appendChild(row);
-
 });
